a3aec3dcbd14e3b69d71f9bf8ab247492aac4ce3
Chandan <chandan@gmail.com>
2024-06-08 22:45:28 +0530

    Separated from backend

--- backend/routes/interviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const interviewController = require('../controllers/interviewController');
-
-router.post('/', interviewController.addInterviewQuestions);
-router.get('/:subject', interviewController.getInterviewQuestionsBySubject);
-
-module.exports = router;
